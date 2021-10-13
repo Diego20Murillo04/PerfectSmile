005bf456d87376691f975c50fbc2b94c1b6b8cc8
Andrés Felipe Padilla López <andrex.palop@gmail.com>
2021-10-13 15:34:40 -0500

    actualizaciones reunion con cliente

---
+++ envioformulario.php
@@ -0,0 +1,17 @@
+<?php
+
+var_dump($_POST);
+
+$name = $_POST["fullname"];
+
+$email = $_POST["email"];
+
+$subjet = $_POST["subject"];
+
+$phone = $_POST["number"];
+
+$message = $_POST["message"];
+
+echo $name." ".$email. " " .$subjet." ".$phone. " ".$message;
+
+?>
